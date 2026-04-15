5fe207c48b1974b29b6106f952d513b4a848a4f7
Omar Shaaban <omar@vrtx.sa>
2026-04-15 23:39:35 +0300

    release omono v0.11.0

--- omono/app/build.gradle.kts
+++ omono/app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
 
     defaultConfig {
         applicationId = "net.omarss.omono"
-        versionCode = 13
-        versionName = "0.10.0"
+        versionCode = 14
+        versionName = "0.11.0"
 
         buildConfigField("String", "TOMTOM_API_KEY", "\"${tomtomApiKey}\"")
     }
