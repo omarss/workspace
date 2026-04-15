93e3822b7c0a4bf60ecc705da4d81b918021b74a
Omar Shaaban <omar@vrtx.sa>
2026-04-15 10:33:36 +0300

    release omono v0.3.0

--- omono/app/build.gradle.kts
+++ omono/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
 
     defaultConfig {
         applicationId = "net.omarss.omono"
-        versionCode = 3
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.3.0"
     }
 
     buildFeatures {
