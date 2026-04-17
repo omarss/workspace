da200e6378ee03952dab40b96238f83c3d917536
Omar Shaaban <omar@vrtx.sa>
2026-04-17 21:37:21 +0300

    release omono v0.17.0

--- omono/app/build.gradle.kts
+++ omono/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
 
     defaultConfig {
         applicationId = "net.omarss.omono"
-        versionCode = 23
-        versionName = "0.16.0"
+        versionCode = 24
+        versionName = "0.17.0"
 
         buildConfigField("String", "TOMTOM_API_KEY", "\"${tomtomApiKey}\"")
         buildConfigField("String", "GPLACES_API_URL", "\"${gplacesApiUrl}\"")
