7dddfd3a7b70390d6c90e78a024694fc2a5f3ee8
Omar Shaaban <omar@vrtx.sa>
2026-04-18 20:31:34 +0300

    release omono v0.32.0

--- omono/app/build.gradle.kts
+++ omono/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
 
     defaultConfig {
         applicationId = "net.omarss.omono"
-        versionCode = 44
-        versionName = "0.31.1"
+        versionCode = 45
+        versionName = "0.32.0"
 
         buildConfigField("String", "GPLACES_API_URL", "\"${gplacesApiUrl}\"")
         buildConfigField("String", "GPLACES_API_KEY", "\"${gplacesApiKey}\"")
